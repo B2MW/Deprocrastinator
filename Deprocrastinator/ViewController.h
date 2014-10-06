b7cb1f1167afca0454730a8b3cbc9a5c8dbbdfbb
B2MW <P5@Champion.local>
2014-10-06 13:39:20 -0500

    Initial Commit

---
+++ Deprocrastinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deprocrastinator
+//
+//  Created by Bradley Walker on 10/6/14.
+//  Copyright (c) 2014 BlackSummerVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
